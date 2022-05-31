fec875825ebc089a8a684cee598d56bb55603b0d
Eyal Bahar <80768687+Eyal-Bahar@users.noreply.github.com>
2022-05-31 20:01:44 +0300

    Update and rename hangman.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-# Write your code here
+"""
+Hangman is a popular yet grim puzzle game. 
+A cruel computer hides a word from you, which you try to guess letter by letter. If you fail, you'll be “hanged”. 
+If you win, you'll survive. You’ve probably played the game at least once or twice. 
+Are you ready to play ?? 
+"""
 import random
 
 class Ui:
